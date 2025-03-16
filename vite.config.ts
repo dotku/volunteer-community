7f32f8ccde52ee296b9e74de48a7867d98c02bdc
Weijing Jay Lin <weijingjaylin@gmail.com>
2025-03-16 02:07:20 -0700

    update the default image folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "./static",
   base: process.env.BASE_URL || "/",
   css: {
     postcss: {
